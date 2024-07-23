3979d8a6113c641a863f46a525c69814b1c989a8
root <root@ip-172-31-2-73.eu-west-1.compute.internal>
2024-07-23 14:19:50 +0000

    Issue fixed2

--- clone_repo.py
+++ clone_repo.py
@@ -1,13 +1,18 @@
-import git
+import os
+import sys
+import subprocess
 
-def clone_repository(repo_url, destination):
+def clone_repo(repo_url, target_dir):
     try:
-        git.Repo.clone_from(repo_url, destination)
-        print(f"Repository cloned to {destination}")
-    except Exception as e:
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+        subprocess.run(['git', 'clone', repo_url, target_dir], check=True)
+        print(f"Repository cloned successfully into {target_dir}")
+    except subprocess.CalledProcessError as e:
         print(f"Error cloning repository: {e}")
 
 if __name__ == "__main__":
-    repo_url = "https://github.com/davidkapcsandi/test1.git"
-    destination = "/home/ec2-user/repo"
-    clone_repository(repo_url, destination)
+    repo_url = 'https://github.com/davidkapcsandi/test1.git'
+    target_dir = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+    clone_repo(repo_url, target_dir)
+
